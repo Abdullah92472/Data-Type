9682f4237e05c795901cd486bbc70ab9fe71b50f
Abdullah Al Imran <abdullah.imran.ete@gmail.com>
2022-02-21 20:13:10 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//String
+let p = "Zahra";
+console.log(typeof(p));
+
+//Integer
+let q = 5;
+console.log(typeof(q));
+
+//Boolean
+let x = true;
+console.log(typeof(x));
+
+//Arrayn, mhnk,j
+const person = {
+    First_name: "Abdullah",
+    Last_name: "Al Imran",
+    Age: 29,
+}
+console.log(typeof(person));
+
+
+//Arrayn, mhnk,j
+const lover = {
+    First_name: "Zahra",
+    Last_name: "Nasr",
+    Age: 28,
+}
+console.log(lover);
